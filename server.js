9c9537b548fe984cb7aa7f29f30d216d7a13fbcf
abdissa-jpeg <abdissa.degefu@aait.edu.et>
2024-05-26 23:14:15 +0300

    Initialization of the webhook

---
+++ server.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const {MONGO_URL} = require('./config').default;
+process.on('uncaughtException', err => {
+    console.log("err: ", err)
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    process.exit(1);
+})
+
+const app = require('./app');
+
+// Database connection
+mongoose.connect(MONGO_URL)
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((err) => {
+    console.log("Database connection failed", err);
+  });
+
+  const port = 3000;
+
+  const server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+  });
+  
+  process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
+  });
